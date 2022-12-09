1d655e630dac58cc8004d39db54c633c0397bd4d
CaterinaAresti <111961519+CaterinaAresti@users.noreply.github.com>
2022-12-09 13:36:45 +0100

    added contact page.php + contact page.css

---
+++ contact_page_e3t.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head> 
+		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+		<title> E3T Contact us </title>
+	</head>
+	<body> 
+		<header>
+			<h1> header </h1>
+		</header>
+		<main>
+		<form method="GET" action="contact_page_e3t.php">
+		<label for="name"> Name </label>
+		<input type="text" name="name">
+		<label for="email"> Email </label>
+		<input type="email" name="email">
+		<label for="comment"> Comment </label>
+		<textarea name="comment">
+		</form>
+		</main>
+		<footer>
+			<h1> footer </h1>
+		</footer>
+	</body>
+</html>
