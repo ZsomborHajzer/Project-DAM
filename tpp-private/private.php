4c0401f14b0e57056b34ee36780d6421f9911d93
ZsomborHajzer <zsombor.hajzer@student.nhlstenden.com>
2022-12-09 14:30:26 +0000

    further format changes to meet coding conventions

--- tpp-private/private.php
+++ tpp-private/private.php
@@ -1,10 +1,16 @@
 <?php
+/*
+This is the private talent page where seperate talents will have different seperate personal pages
+This is the area where they can upload images of themselfs and their own documnets. They are also able to 
+delete these documents.  Profile Image will aslo be changeable. Styling for this page is not a focus. 
+The aim of this page is customization.
+*/
+
 $arrayofImages = [];
-$pictureholder = "pictureholder/";
-$documentholder = "documentholder/";
+$pictureHolder = "pictureholder/";
+$documentHolder = "documentholder/";
 $files = [];
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -18,6 +24,7 @@ $files = [];
 </head>
 
 <body>
+
     <div class="container">
 
         <header>
@@ -32,19 +39,19 @@ $files = [];
             <p>Insert talent fname/lname</p>
             <p><b>Specialities: </b>Guitar, Singer, Songwriter, Actor </p>
         </div>
+
         <div class="email">
             <p>Email address: <a href="mailto:testemail@adus.com">testemail@adus.com</a></p>
         </div>
 
-        <div class="phototitle">
+        <div class="photoTitle">
             <h1><b>Photos</b></h1>
         </div>
 
-
-        <div class="photoholder">
+        <div class="photoHolder">
 
             <!--  first img should be this one for talents so they can add more images later on to the project  -->
-            <div class="addimg">
+            <div class="addImg">
                 <form action="#" method="post" enctype="multipart/form-data">
                     <label>
                         <input type="file" name="uploadImg" id="" style="display:none">
@@ -54,35 +61,30 @@ $files = [];
                         </figcaption>
                     </label>
             </div>
-            <!--  <div class="addimg">
-                <img src="addimg/addimg.png" alt="" height=200 width=200>
-                <figcaption>
-                    <p>Add a new image</p>
-                </figcaption>
-            </div> -->
+
             </form>
 
             <?php
 
             // checks if there is a directory in that address or not
-            if (is_dir($pictureholder)) {
+            if (is_dir($pictureHolder)) {
 
                 // if there is a directory, scan the names of the files in that directory and put it in a array called $array of images
-                $arrayofImages = scandir($pictureholder);
+                $arrayofImages = scandir($pictureHolder);
 
-                //count the number of pictures in the array
-                //exclude the ones called '.' and '..' cuz those are invisible for the user
+                // count the number of pictures in the array
+                // exclude the ones called '.' and '..' cuz those are invisible for the user
                 // push the names of the files with  complete location this time to another array called $files
                 for ($i = 0; $i < count($arrayofImages); $i++) {
                     if ($arrayofImages[$i] != '.' && $arrayofImages[$i] != '..') {
 
-                        array_push($files, $pictureholder . $arrayofImages[$i]);
+                        array_push($files, $pictureHolder . $arrayofImages[$i]);
                     }
                 }
 
                 // sort files by last modified date
                 // orders them by most recently modified on top and last modified on bottom
-                //renaming files does not count as modification apperantly
+                // renaming files does not count as modification apperantly
 
                 usort(
                     $files,
@@ -95,7 +97,7 @@ $files = [];
                 foreach ($files as $item) {
                     echo "<div class='pic'>";
                     echo "<img src=" . $item . " height = 200  width = 200 />";
-                    echo '<figcaption> <p>Variable input from </br> user limited to a few words</p> </figcaption>';
+                    echo "<figcaption> <p>Variable input from </br> user limited to a few words</p> </figcaption>";
                     echo "</div>";
                 }
             }
@@ -104,7 +106,7 @@ $files = [];
 
         </div>
 
-        <div class="documentstitle">
+        <div class="documentsTitle">
             <h2>Documents</h2>
         </div>
 
@@ -113,18 +115,16 @@ $files = [];
             <?php
 
             // check if documentholder location exists
-            if (is_dir($documentholder)) {
-
+            if (is_dir($documentHolder)) {
 
                 // puts the name of the docs into the array $arrayofDocs
-                $arrayofDocs = scandir($documentholder);
+                $arrayofDocs = scandir($documentHolder);
 
-                //count the number of docs in the array (starts at two because of the invisible docs '.' and '..')
+                // count the number of docs in the array (starts at two because of the invisible docs '.' and '..')
                 for ($i = 2; $i < count($arrayofDocs); $i++) {
 
-
-                    //html gibberish
-                    echo '<div class="docpics">';
+                    // html gibberish
+                    echo '<div class="docPics">';
                     echo "<a href='documentholder/doc1.docx' download><img src='stockphotoholder/docxstockphoto.png'  height = 250  width = 200 /></a>";
                     echo '<figcaption>' . $arrayofDocs[$i] . '</figcaption>';
                     echo '</div>';
@@ -133,7 +133,7 @@ $files = [];
 
             ?>
 
-            <div class="adddoc">
+            <div class="addDoc">
                 <form action="#" method="post" enctype="multipart/form-data">
                     <label>
                         <input type="file" name="uploadDoc" id="" style="display:none">
