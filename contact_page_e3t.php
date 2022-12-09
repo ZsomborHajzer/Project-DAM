7e5ec2e52aeb775438c6135086d6392146c2937a
CaterinaAresti <111961519+CaterinaAresti@users.noreply.github.com>
2022-12-09 15:01:57 +0100

    added contact info

--- contact_page_e3t.php
+++ contact_page_e3t.php
@@ -6,21 +6,50 @@
 		<title> E3T Contact us </title>
 	</head>
 	<body> 
-		<header>
-			<h1> header </h1>
-		</header>
-		<main>
-		<form method="GET" action="contact_page_e3t.php">
-		<label for="name"> Name </label>
-		<input type="text" name="name">
-		<label for="email"> Email </label>
-		<input type="email" name="email">
-		<label for="comment"> Comment </label>
-		<textarea name="comment">
-		</form>
-		</main>
-		<footer>
-			<h1> footer </h1>
-		</footer>
+		<div id="container">
+			<header>
+				<h1> header </h1>
+			</header>
+			<main>
+			<div id="form">
+				<h2> contact us </h2>
+				<form method="GET" action="contact_page_e3t.php">
+					<p class="labels"> <label for="name"> Name </label> </p>
+					<p class="texts"> <input type="text" name="name"> </p>
+					<p class="labels"> <label for="email"> Email </label> </p>
+					<p class="texts"> <input type="email" name="email"> </p>
+					<p class="labels"> <label for="comment"> Comment </label> </p>
+					<p class="texts"> <textarea name="comment"> </textarea> </p>
+					<p id="submit"> <input type="submit" value="send"> </p>
+				</form>
+			</div>
+			<div id="contactinfo">
+				<div class="contactbox">
+					<img src="pictures/contact1.jpg" height="130px" width="100px">
+					<h3> FirstName LastName </h3>
+					<p class="texts"> manager </p>
+					<p> name@mail.com </p>
+					<p> +12 345 6789 </p>
+				</div>
+				<div class="contactbox">
+					<img src="pictures/contact2.jpg" height="130px" width="100px">
+					<h3> FirstName LastName </h3>
+					<p class="texts"> manager </p>
+					<p> name@mail.com </p>
+					<p> +12 345 6789 </p>
+				</div>
+				<div class="contactbox">
+					<img src="pictures/contact3.jpg" height="130px" width="100px">
+					<h3> FirstName LastName </h3>
+					<p class="texts"> manager </p>
+					<p> name@mail.com </p>
+					<p> +12 345 6789 </p>
+				</div>
+			</div>
+			</main>
+			<footer>
+				<h1> footer </h1>
+			</footer>
+		</div>
 	</body>
 </html>
