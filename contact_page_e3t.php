eb0784d10a217b44dcacd67d354717ce2e52feab
CaterinaAresti <111961519+CaterinaAresti@users.noreply.github.com>
2022-12-14 14:07:03 +0100

    Add: php code to make the form work

--- contact_page_e3t.php
+++ contact_page_e3t.php
@@ -14,7 +14,7 @@
 	
 	<div id="form">
 		<h2> contact us </h2>
-		<form method="GET" action="contact_page_e3t.php">
+		<form method="POST" action="contact_page_e3t.php">
 			<p class="labels"> <label for="dataName"> Name </label> </p>
 			<p class="texts"> <input type="text" name="dataName"> </p>
 			<p class="labels"> <label for="dataEmail"> Email </label> </p>
@@ -24,6 +24,40 @@
 			<p id="submit"> <input type="submit" value="send" id="submit2"> </p>
 		</form>
 	</div>
+	
+	<?php
+	
+	if($_SERVER['REQUEST_METHOD']=="POST"){
+		$dataName = filter_input(INPUT_POST,"dataName");
+		$dataEmail = filter_input(INPUT_POST,"dataEmail");
+		$dataComment = filter_input(INPUT_POST,"dataComment");
+		
+		$errors = array();
+		$flag = 0;
+		
+		if($dataEmail = filter_input (INPUT_POST, "dataEmail", FILTER_VALIDATE_EMAIL)){
+			echo "";
+		}
+		else {
+			array_push($error, "<p> Your email should contain an '@' and end with '.com' </p>");
+			$flag = 1;
+		}
+		
+		if(str_word_count($dataComment) < 5) {
+			array_push($error, "<p> Your comment should contain a minimum of 5 words </p>");
+			$flag = 1;
+		}
+		
+		if($flag == 0){
+            echo "<p> Hello $dataName, your comment has been sent successfully. Thank you! </p>";
+        }
+        else{
+            foreach($errors as $error){
+                echo $error;
+            }
+        }
+	}
+	?>
 	
 	<div id="contactinfo">
 		<div class="contactbox">
