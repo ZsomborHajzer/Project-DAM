f30fa7a8d85b0b5d14ac4c3a38d77ee0507dcfde
ZsomborHajzer <zsombor.hajzer@student.nhlstenden.com>
2022-12-19 10:59:51 +0000

    Added Photo addition functionality

--- tpp-private/public/view/private.php
+++ tpp-private/public/view/private.php
@@ -75,28 +75,42 @@ $fileSize = 4 * 1024 * 1024; //4MB
         <?php
 
         require "../../components/profileUpload.php";
-
         ?>
+
 
         <div class="photoTitle">
             <h1><b>Photos</b></h1>
         </div>
 
-        <div class="photoHolder">
+        < <div class="photoHolder">
 
             <!--  first img should be this one for talents so they can add more images later on to the project  -->
-            <div class="addImg">
-                <form action="#" method="post" enctype="multipart/form-data">
+
+            <form action="" method="POST" enctype="multipart/form-data" name="yes">
+
+
+                <div class="addImg">
                     <label>
-                        <input type="file" name="uploadImg" id="" style="display:none">
+                        <input type="file" name="uploadImg" onchange="this.form.submit()" style=" display:none">
                         <img src=<?php echo $stockPhotoLocation; ?> alt="addimg" id="stockphotoAddImg">
                         <figcaption>
                             <p>Add a new image</p>
                         </figcaption>
                     </label>
-            </div>
+                </div>
 
             </form>
+
+            <?php
+            // if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            //     if (isset($_GET["uploadImg"])) {
+            require_once "../../components/photoAdd.php";
+            //       }
+            // }
+
+
+            ?>
 
             <?php
 
@@ -138,52 +152,58 @@ $fileSize = 4 * 1024 * 1024; //4MB
 
             ?>
 
-        </div>
+    </div>
 
-        <div class="documentsTitle">
-            <h2>Documents</h2>
-        </div>
+    <div class="documentsTitle">
+        <h2>Documents</h2>
+    </div>
 
-        <div class="documents">
+    <div class="documentAdd">
+        <form action="" method="post" enctype="multipart/form-data">
+            <input type="submit" value="Add New Documnet" name="documentAdd">
+        </form>
+    </div>
 
-            <?php
+    <div class="documents">
 
-            // check if documentholder location exists
-            if (is_dir($documentHolder)) {
+        <?php
 
-                // puts the name of the docs into the array $arrayofDocs
-                $arrayofDocs = scandir($documentHolder);
+        // check if documentholder location exists
+        if (is_dir($documentHolder)) {
 
-                // count the number of docs in the array (starts at two because of the invisible docs '.' and '..')
-                for ($i = 2; $i < count($arrayofDocs); $i++) {
+            // puts the name of the docs into the array $arrayofDocs
+            $arrayofDocs = scandir($documentHolder);
 
-                    // html gibberish
-                    echo '<div class="docPics">';
-                    echo "<a href='" . $documentHolder . $arrayofDocs[$i] . "' download><img src='" . $stockDocumentLocation . "/docxstockphoto.png'  height = 250  width = 200 /></a>";
-                    echo '<figcaption>' . $arrayofDocs[$i] . '</figcaption>';
-                    echo '</div>';
-                }
+            // count the number of docs in the array (starts at two because of the invisible docs '.' and '..')
+            for ($i = 2; $i < count($arrayofDocs); $i++) {
+
+                // html gibberish
+                echo '<div class="docPics">';
+                echo "<a href='" . $documentHolder . $arrayofDocs[$i] . "' download><img src='" . $stockDocumentLocation . "/docxstockphoto.png'  height = 250  width = 200 /></a>";
+                echo '<figcaption>' . $arrayofDocs[$i] . '</figcaption>';
+                echo '</div>';
             }
+        }
 
-            ?>
+        ?>
 
-            <!--  add doc image on the bottom -->
-            <div class="addDoc">
-                <form action="#" method="post" enctype="multipart/form-data">
-                    <label>
-                        <input type="file" name="uploadDoc" id="" style="display:none">
-                        <img src=<?php echo $addDocLocation; ?> alt="" height="200" width="200">
-                        <figcaption>
-                            <p>Add a new document</p>
-                        </figcaption>
-                    </label>
-            </div>
-
+        <!--  add doc image on the bottom -->
+        <div class="addDoc">
+            <form action="#" method="post" enctype="multipart/form-data">
+                <label>
+                    <input type="file" name="uploadDoc" id="" style="display:none">
+                    <img src=<?php echo $addDocLocation; ?> alt="" height="200" width="200">
+                    <figcaption>
+                        <p>Add a new document</p>
+                    </figcaption>
+                </label>
         </div>
 
-        <footer>
-            <h1>Placeholder for actuall footer</h1>
-        </footer>
+    </div>
+
+    <footer>
+        <h1>Placeholder for actuall footer</h1>
+    </footer>
 
     </div>
 
