8d42fdb1f7baa28e0556d41206f89feebdb6d298
Chris <ayomide.olagoke@student.nhlstenden.com>
2023-01-09 16:45:04 +0100

    UPDATE Admin page completed

--- adminPage.php
+++ adminPage.php
@@ -1,9 +1,11 @@
-
+<?php 
+global $dEmail;
+?>
 <!DOCTYPE HTML>
 <html lang="en">
 	<head>
 		<title>AdminPage</title>
-		<link rel="stylesheet" href="css/adminPageCss.css" />
+		<!-- <link rel="stylesheet" href="css/adminPageCss.css" /> -->
 	</head>
 
 	<body>
@@ -11,41 +13,108 @@
 			<div class="formBox">
 				<div id="emailSearchLabel"><label  for="talentEmailInput">Search by email</label></div>
 				<div id="talentEmailInputDiv"><input id="talentEmailInput"  type="email" name="dataEmail" placeholder=""></div>
+				<INPUT type="hidden" name="action" value="search">
 				<button id="submitTalentEmail" type=submit>Enter</button>
 			</div>
 		</form>
-
+		<div class='talentProfile'>
 		<?php 
 		// Create retrieval of talent information from email entered, check if email entered exists
-		
+		$err=[];
 		$dsn="mysql:host=localhost;dbname=dbprojectterm2";
 		$user="root";
 		$passwrd="";
 
 		$dbHandler = new PDO($dsn, $user, $passwrd);
+
+		if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["action"] == "search"){
+
+			if(!$email = filter_input(INPUT_POST, "dataEmail", FILTER_VALIDATE_EMAIL)){
+				$err[] ="Invalid email";
+			}
+
+			$query= "SELECT * FROM tbluser WHERE dtEmail=:dtEmail";
+			$getinfo= $dbHandler-> prepare($query);
+			$getinfo->bindParam("dtEmail", $email);
+			$getinfo->execute();
+
+			if(isset($getinfo)){
+				$row = $getinfo->fetch();
+				$dEmail=$row["dtEmail"];
+				echo "global email is ".$dEmail;
+				echo "<img id='talentProfilePicture' src=".$row["dtImage"].">";
+				echo "<p id='talentName'>".$row["dtFirstName"]." ".$row["dtLastName"]."</p>";
+				echo "<p id='talentEmail'>".$row["dtEmail"]."</p>";
+				echo "<p id='talentPassword'>".$row["dtPassword"]."</p>";
+				echo "<p id='talentRating'>"."Rating: ***"."</p>";
+
+				
+			}else{
+				echo "email entered is not in database";
+			}
+		
+		
 		?>
-
-
-		<div class="profileBox">
-			<div class="talentProfile">
-				<img id="talentProfilePicture" src="imagePlaceholder/placeholder.jpg" alt="placeholder">
-				<p id="talentName">Kratos</p>
-				<p id="talentEmail">GodOfWar@gmail.com</p>
-				<p id="talentPassword">Password</p>
-				<p id="talentRating">Rating:****</p>
-
+		
+ 	<div id="activityButtons">
+			<form method="POST" action="adminPage.php" >
+                <label for="inActive">inActive</label><input type="radio" name="active" value="0" <?php echo ($row["dtActive"] == 0 ? "checked" : ""); ?>>
+                <label for="active">Active</label><input  type="radio" name="active" value="1" <?php echo ($row["dtActive"] == 1 ? "checked" : ""); ?>>
 			</div>
-
-			<div id="activityButtons">
-			<form action="adminpage.php" method="POST">
-			<input type="button" id="inactiveButton" value="Make Inactive">
-			<input type="button" id="activeButton" value="Make Active">
+			<div>
+			<input type="hidden" name="action" value="change">
+			<input type="hidden" name="email" value="<?= $dEmail ?>">
+				<button type="submit">Enter</button>
 			</div>
 			</form>
+
 			<?php
+
+			}
+
 			//Create php to change user from active to inactive & vice versa
-			?>
-		</div>
+            if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["action"] == "change") {
+               
+				$dEmail = $_POST["email"];
+				if(!empty($dEmail)){
+
+					try {
+				// if(isset($_POST["active"])){
+				// 	$active= $_POST["active"];
+				// }
+				
+				// if(isset($_POST["inActive"])){
+				// 	$active= $_POST["inActive"];
+				// }
+
+				$active = (int)$_POST["active"];
+				
+
+				var_dump($active);
+				echo "<p>debug Line 73</p>";
+				
+                    $sql1 = "UPDATE tblUser SET dtActive = :active WHERE dtEmail like :email";
+                    $stmt = $dbHandler->prepare($sql1);
+					$stmt->bindParam("active", $active);
+					$stmt->bindParam("email", $dEmail);
+                    $stmt->execute();
+                 
+                    echo "Talent activity has been changed";
+
+				}catch (Exception $exception){
+					echo "not success";
+					echo $exception;
+				}
+			}
+			else {
+				echo "no email";
+			}
+
+				
+			}
+			
+            ?>
+	</div>
 	</body>
 
 
