bb57bbdae3544cd9014df9b169f22d08a334c467
Alves Bernardo <bernardo.correia.alves@student.nhlstenden.com>
2023-01-10 12:04:55 +0100

    Fix: Only allow booking from dates beginning today

--- html/Booking.php
+++ html/Booking.php
@@ -54,6 +54,27 @@
                     
                 }
             })
+
+            $(function(){
+                var dtToday = new Date();
+
+                var month = dtToday.getMonth() + 1;
+                var day = dtToday.getDate();
+                var year = dtToday.getFullYear();
+                if(month < 10)
+                    month = '0' + month.toString();
+                if(day < 10)
+                    day = '0' + day.toString();
+
+                var maxDate = year + '-' + month + '-' + day;
+
+                // or instead:
+                // var maxDate = dtToday.toISOString().substr(0, 10);
+
+
+
+                $('#dataDate').attr('min', maxDate);
+            });
         });
 
 
@@ -136,7 +157,7 @@
                 <div class="gen-content">
                     <?php
 
-                    $dsn = "mysql:host=localhost;dbname=databaseproject2";
+                    $dsn = "mysql:host=localhost;dbname=dbprojectterm2";
                     $user = "root";
                     $passwd = "";
 
