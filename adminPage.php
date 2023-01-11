f6f2160a851ee85965903e4c99f315e36778f16d
RaresManoli <rares.manoli@student.nhlstenden.com>
2023-01-11 23:53:49 +0200

    Fix: admin page functionality

--- adminPage.php
+++ adminPage.php
@@ -1,35 +1,33 @@
 <?php 
     global $dEmail;
+
+
+
 ?>
 <!DOCTYPE HTML>
 <html lang="en">
 	<head>
 		<title>AdminPage</title>
-		 <link rel="stylesheet" href="adminPageCss.css">
+		 <link rel="stylesheet" href="css/adminPageCss.css">
 	</head>
     <script>
-            function showmore() {
-                var submitEmailForm = document.getElementById("submitEmailForm")
+            function showmore(id) {
+                var submitEmailForm = document.getElementById("submitEmailForm" + id)
                 if (submitEmailForm.classList[0] === 'd-none') {
                     submitEmailForm.classList.remove("d-none");
                     submitEmailForm.classList.add("d-inline");
+                } else {
+                    submitEmailForm.classList.remove("d-inline");
+                    submitEmailForm.classList.add("d-none");
                 }
             }
     </script>
 	<body>
         <main>
-            <form id="emailSearch" action="adminPage.php" method="POST">
-                <div class="formBox">
-                    <div id="emailSearchLabel"><label  for="talentEmailInput">Find Talent by email</label></div>
-                    <div id="inputSendButton">
-                        <div id="talentEmailInputDiv"><input id="talentEmailInput"  type="email" name="dataEmail" placeholder=""></div>
-                        <input type="hidden" name="action" value="search">
-                        <button id="submitTalentEmail" type=submit>Enter</button>
-                    </div>
-                </div>
-            </form>
+
+
             <div id="mainTalentInformation">
-                <div class='talentProfile'>
+
                 <?php
                 // Create retrieval of talent information from email entered, check if email entered exists
                 $err=[];
@@ -39,98 +37,47 @@
 
                 $dbHandler = new PDO($dsn, $user, $passwrd);
 
-                if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["action"] == "search"){
+                //Create php to change user from active to inactive & vice versa
+                if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["action"] == "change") {
 
-                    if(!$email = filter_input(INPUT_POST, "dataEmail", FILTER_VALIDATE_EMAIL)){
-                        $err[] ="Invalid email";
-                    }
+                    $userId = $_POST["userId"];
+                    if(!empty($userId)){
 
-                    $query= "SELECT * FROM tblUser WHERE dtEmail=:dtEmail";
-                    $getinfo= $dbHandler-> prepare($query);
-                    $getinfo->bindParam("dtEmail", $email);
-                    $getinfo->execute();
+                        try {
+                            // if(isset($_POST["active"])){
+                            // 	$active= $_POST["active"];
+                            // }
 
-                    if(isset($getinfo)){
-                        $row = $getinfo->fetch();
-                        $dEmail=$row["dtEmail"];
-                        //echo "global email is ".$dEmail;
-                        echo "<img id='talentProfilePicture' src=".$row["dtImage"].">";
-                        echo '<div id="talentInformation">';
-                        echo "<p id='talentName'>".$row["dtFirstName"]." ".$row["dtLastName"]."</p>";
-                        echo "<p id='talentEmail'>".$row["dtEmail"]."</p>";
-                        echo "<p id='talentPassword'>".str_repeat("*", strlen($row["dtPassword"]))."</p>";
-                        echo '</div>';
-                        echo '<div id="emailButton">';
-                        echo "<button id='changeEmailButton' name='changeEmail' onclick='showmore()'>Change Email</button>";
-                        echo '</div>';
-                        echo '<div id="submitEmailSearchBar">';
-                        echo "<form action='adminPage.php' id='submitEmailForm' class='d-none' method='post'><button id='changeEmailSubmit' type='submit'>submit Email</button>";//the button that submits the email
-                        echo "<input id='newEmail' placeholder='type the new email here' type='email' name='newEmail'><input type='hidden' name='action' value='newEmail'><input type='hidden' name='oldEmail' value='".$row["dtEmail"]."'> </form>";
-                        echo '</div>';
+                            // if(isset($_POST["inActive"])){
+                            // 	$active= $_POST["inActive"];
+                            // }
+
+                            $active = (int)$_POST["active"];
 
 
-                    }else{
-                        echo "email entered is not in database";
-                    }
 
 
-                ?>
-                    <div id="submitChange">
-                        <input type="hidden" name="action" value="change">
-                        <input type="hidden" name="email" value="<?= $dEmail ?>">
-                        <button id="radioSubmitButton" type="submit">Enter</button>
-                    </div>
-            <div id="radioButtons">
-                    <form method="POST" action="adminPage.php" >
-                        <label for="Inactive">Inactive</label><input type="radio" id="inactiveButton" name="active" value="0" <?php echo ($row["dtActive"] == 0 ? "checked" : ""); ?>>
-                        <label for="active">Active</label><input  type="radio" id="activeButton" name="active" value="1" <?php echo ($row["dtActive"] == 1 ? "checked" : ""); ?>>
-                    </div>
-                    </form>
 
-                    <?php
-
-                    }
-
-                    //Create php to change user from active to inactive & vice versa
-                    if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["action"] == "change") {
-
-                        $dEmail = $_POST["email"];
-                        if(!empty($dEmail)){
-
-                            try {
-                        // if(isset($_POST["active"])){
-                        // 	$active= $_POST["active"];
-                        // }
-
-                        // if(isset($_POST["inActive"])){
-                        // 	$active= $_POST["inActive"];
-                        // }
-
-                        $active = (int)$_POST["active"];
-
-
-                        var_dump($active);
-                        echo "<p>debug Line 73</p>";
-
-                            $sql1 = "UPDATE tblUser SET dtActive = :active WHERE dtEmail like :email";
+                            $sql1 = "UPDATE tblUser SET dtActive = :active WHERE idUser like :id";
                             $stmt = $dbHandler->prepare($sql1);
                             $stmt->bindParam("active", $active);
-                            $stmt->bindParam("email", $dEmail);
+                            $stmt->bindParam("id", $userId);
                             $stmt->execute();
 
-                            echo "Talent activity has been changed";
+                            echo '<script>alert("Talent activity has been changed")</script>';
 
                         }catch (Exception $exception){
-                            echo "not success";
+                            echo '<script>alert("Unexpected error")</script>';
                             echo $exception;
                         }
                     }
                     else {
-                        echo "no email";
+                        echo '<script>alert("No id was specified")</script>';
                     }
 
 
-                    }
+                }
+
                 //Create php to change user from active to inactive & vice versa
                 if($_SERVER["REQUEST_METHOD"] == "POST" && $_POST["action"] == "newEmail") {
 
@@ -148,19 +95,78 @@
                             $stmt->bindParam("oldEmail", $oldEmail);
                             $stmt->execute();
 
-                            echo "Talent Email has been changed";
+
+                            echo '<script>alert("Successfully changed")</script>';
+
 
                         }catch (Exception $exception){
-                            echo "not success";
+                            echo '<script>alert("Not success")</script>';
                             echo $exception;
                         }
                     }
                     else {
-                        echo "no email";
+                        echo '<script>alert("No email")</script>';
                     }
 
 
                 }
+
+
+
+
+                    $query= "SELECT * FROM tblUser WHERE dtIsAdmin=0 ";
+                    $getinfo= $dbHandler-> prepare($query);
+                    $getinfo->execute();
+
+
+                       // $row = $getinfo->fetch();
+                        //$dEmail=$row["dtEmail"];
+                        while ($row = $getinfo->fetch()) {
+                        $isActive = $row["dtActive"];
+                        $userId = $row["idUser"];
+
+                        echo "<div class='talentProfile'>";
+                        echo '<div id="talentInformation">';
+                        echo "<img id='talentProfilePicture' src=".$row["dtImage"].">";
+                        echo "<p id='talentName'>".$row["dtFirstName"]." ".$row["dtLastName"]."</p>";
+                        echo "<p id='talentEmail'>".$row["dtEmail"]."</p>";
+                        echo "<p id='talentPassword'>".str_repeat("*", strlen($row["dtPassword"]))."</p>";
+                        echo '</div>';
+                        echo '<div id="emailButton">';
+                        echo "<button id='changeEmailButton' name='changeEmail' onclick='showmore($userId)'>Change Email</button>";
+                        echo '</div>';
+                        echo '<div id="submitEmailSearchBar">';
+                        echo "<form action='adminPage.php' id='submitEmailForm$userId' class='d-none' method='post'><button id='changeEmailSubmit' type='submit'>submit Email</button>";//the button that submits the email
+                        echo "<input id='newEmail' placeholder='type the new email here' type='email' name='newEmail'><input type='hidden' name='action' value='newEmail'><input type='hidden' name='oldEmail' value='".$row["dtEmail"]."'> </form>";
+                        echo '</div>';
+                        echo '<div id="radioButtons">
+                    <form method="POST" action="adminPage.php" >
+                        <label for="Inactive">Inactive</label><input type="radio" id="inactiveButton" name="active" value="0" ' . ($isActive == 0 ? 'checked' : "") . '>
+                        <label for="active">Active</label><input  type="radio" id="activeButton" name="active" value="1" ' . ($isActive == 1 ? 'checked'  :"").'>
+                        <input type="hidden" name="action" value="change">
+                        <input type="hidden" name="userId" value="' . $row["idUser"] . '"> 
+                        
+
+                </div>
+                 <div id="submitChange">
+                        
+                  <button id="radioSubmitButton" type="submit">Enter</button>
+                    </div></form>';
+                        echo "</div>";
+                    }
+
+
+
+
+
+                ?>
+
+                    <?php
+
+
+
+
+
 
                     ?>
             </div>
