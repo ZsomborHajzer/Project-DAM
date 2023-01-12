d0fed7183389a49b8f6094960becd4b96a6dd765
AlvesBernardo <bernardo.correia.alves@student.nhlstenden.com>
2023-01-12 23:37:43 +0100

    Fix: Trying to fix the profil page at nearly 00am do I have a life?

--- public/view/private.php
+++ public/view/private.php
@@ -10,7 +10,7 @@ This is the area where they can upload images of themselfs and their own documne
 delete these documents.  Profile Image will aslo be changeable. Styling for this page is not a focus. 
 The aim of this page is customization.
 */
-
+/*
 //Double refresh the page in order to update the profile picture correctly
 if (isset($_POST["newProfilePic"])) {
     header("Location: private.php");
@@ -29,21 +29,21 @@ if (isset($_POST["docUpload"])) {
 if (isset($_POST["deletePic"])) {
     header("Location: private.php");
 }
-
+*/
 //Database connect
 include "/var/www/E3T/components/dbConnect.php";
-
+/*-
 $arrayofImages = [];
 $pictureHolder = "../img/pictureholder/";
 $documentHolder = "../img/documentholder/";
-$profileImgLocations = "images/profileimg/";
+$profileImgLocations = "../img/profileimg/";
 $stockPhotoLocation = "images/stockphotoholder/addimg.png";
 $addDocLocation = "img/stockphotoholder/adddoc.png";
 $stockDocumentLocation = "img/stockphotoholder/";
 $files = [];
 $profileFiles = [];
 $fileSize = 4 * 1024 * 1024; //4MB
-
+*/
 //Session variables
 $_SESSION["id"];
 
@@ -92,18 +92,11 @@ $_SESSION["id"];
     </script>
 
 
-
     <div class="container">
 
 
 
-        <!-- always echo the first image in the folder "profileimg -->
-        <div class="profileImage">
-            <img src=<?php
-                        $printedProfile = scandir($profileImgLocations);
-                        echo $profileImgLocations . $printedProfile[2];
-                        ?> alt="" width=200 height=200 />
-        </div>
+   
 
         <?php
         //This is query requesting information from user database 
@@ -187,189 +180,6 @@ $_SESSION["id"];
 
         }
         ?>
-        <div class="photoTitle">
-            <h1><b>Photos</b></h1>
-        </div>
-
-        <div class="photoHolder">
-
-            <!--  first img should be this one for talents so they can add more images later on to the project  -->
-
-            <form action="" method="POST" enctype="multipart/form-data" name="yes">
-
-                <div class="addImg">
-                    <label>
-                        <input type="file" name="uploadImg" onchange="this.form.submit()" style=" display:none">
-                        <img src=<?php echo $stockPhotoLocation; ?> alt="addimg" id="stockphotoAddImg">
-                        <input type="hidden" name="imgUpload" value="imgUpload">
-                        <figcaption>
-                            <p>Add a new image</p>
-                        </figcaption>
-                    </label>
-                </div>
-
-            </form>
-
-            <?php
-
-            // checks if there is a directory in that address or not
-            if (is_dir($pictureHolder)) {
-
-                // if there is a directory, scan the names of the files in that directory and put it in a array called $array of images
-                $arrayofImages = scandir($pictureHolder);
-
-                // count the number of pictures in the array
-                // exclude the ones called '.' and '..' cuz those are invisible for the user
-                // push the names of the files with  complete location this time to another array called $files
-                for ($i = 0; $i < count($arrayofImages); $i++) {
-                    if ($arrayofImages[$i] != '.' && $arrayofImages[$i] != '..') {
-
-                        array_push($files, $pictureHolder . $arrayofImages[$i]);
-                    }
-                }
-
-                // sort files by last modified date
-                // orders them by most recently modified on top and last modified on bottom
-                // renaming files does not count as modification apperantly
-
-                usort(
-                    $files,
-                    function ($x, $y) {
-                        return filemtime($y) <=> filemtime($x);
-                    }
-                );
-
-                // echo the html script with file locations
-                foreach ($files as $item) {
-                    echo "<div class='pic'>";
-                    echo "<img src=" . $item . " alt='images' id='images' height='200' width='200' />";
-                    echo "<figcaption> <p>Variable input from </br> user limited to a few words</p> </figcaption>";
-                    echo "</div>";
-                }
-            }
-
-            ?>
-        </div>
-
-        <div class="deletePhotoTitle">
-            <h1><b>Delete a photo</b></h1>
-        </div>
-
-        <div class="deletePhoto">
-            <form action="#" method="post" enctype="multipart/form-data">
-                <select name="deletePic" id="deletePic" value="Delete">
-                    <?php
-                    $arrayofImages = scandir($pictureHolder);
-                    for ($i = 0; $i < count($arrayofImages); $i++) {
-                        if ($arrayofImages[$i] != '.' && $arrayofImages[$i] != '..') {
-                            echo '<option value="' . $arrayofImages[$i] . '">' . $arrayofImages[$i] . '</option>';
-                        }
-                    }
-
-                    ?>
-
-                </select>
-                <input type="submit" value="Delete" name="Delete">
-            </form>
-
-        </div>
-
-
-
-
-        <div class=" documentsTitle">
-            <h1>Documents</h1>
-        </div>
-
-        <div class="documents">
-
-            <?php
-
-            // check if documentholder location exists
-            if (is_dir($documentHolder)) {
-
-                // puts the name of the docs into the array $arrayofDocs
-                $arrayofDocs = scandir($documentHolder);
-
-                // count the number of docs in the array (starts at two because of the invisible docs '.' and '..')
-                for ($i = 2; $i < count($arrayofDocs); $i++) {
-
-                    // html gibberish
-                    echo '<div class="docPics">';
-                    echo "<a href='" . $documentHolder . $arrayofDocs[$i] . "' download><img src='" . $stockDocumentLocation . "/docxstockphoto.png'  height = 250  width = 200 /></a>";
-                    echo '<figcaption>' . $arrayofDocs[$i] . '</figcaption>';
-                    echo '</div>';
-                }
-            }
-
-            ?>
-
-            <!--  add doc image on the bottom -->
-            <div class="addDoc">
-                <form action="#" method="post" enctype="multipart/form-data">
-                    <label>
-                        <input type="file" name="uploadDoc" onchange="this.form.submit()" id="" style="display:none">
-                        <img src=<?php echo $addDocLocation; ?> alt="" height="200" width="200">
-                        <input type="hidden" name="docUpload" value="docUpload">
-                        <figcaption>
-                            <p>Add a new document</p>
-                        </figcaption>
-                    </label>
-            </div>
-
-        </div>
-        <div class="deleteDocumentTitle">
-            <h1><b>Delete a Document</b></h1>
-        </div>
-
-        <div class="deleteDocument">
-            <form action="#" method="post" enctype="multipart/form-data">
-                <select name="deleteDoc" id="deleteDoc" value="Delete">
-                    <?php
-                    $arrayofDocs = scandir($documentHolder);
-                    for ($i = 2; $i < count($arrayofDocs); $i++) {
-                        echo '<option value="' . $arrayofDocs[$i] . '">' . $arrayofDocs[$i] . '</option>';
-                    }
-                    ?>
-                </select>
-                <input type="submit" value="Delete" name="deleteDocSubmit">
-            </form>
-        </div>
-
-
-        <?php
-        //Delete Picture part
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            if (isset($_POST["deletePic"])) {
-                $deletePic = $_POST["deletePic"];
-                @(unlink("../img/pictureholder/" . $deletePic . ""));
-            }
-
-            if (isset($_POST["deleteDocSubmit"])) {
-                print_r($_POST);
-                $deleteDoc = $_POST["deleteDoc"];
-                @(unlink("../img/documentholder/" . $deleteDoc . ""));
-            }
-        }
-
-
-
-
-        //Require files to lessen the amount of code in one page
-        if (isset($_POST["imgUpload"])) {
-            require "/var/www/E3T/components/photoAdd.php";
-        }
-
-        if (isset($_POST["docUpload"])) {
-            require "/var/www/E3T/components/documentAdd.php";
-        }
-
-        if (isset($_POST["newProfilePic"])) {
-            require "/var/www/E3T/components/profileUpload.php";
-        }
-
-        ?>
-
 
 
     </div>
