1103ae047af2afed9d50c9943b28f91939070174
unknown <user@DESKTOP-C4U7FPJ.corp.E3T.com>
2023-01-12 17:57:54 +0000

    fix: Header links

--- components/header.php
+++ components/header.php
@@ -13,11 +13,19 @@ session_start();
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@500;600&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@500;600&display=swap"
+          rel="stylesheet">
     <!-- My css -->
     <link rel="stylesheet" href="css/index.css">
     <link rel="stylesheet" href="css/<?= $cssFile; ?>">
     <!--Font Awesome script-->
     <script src="https://kit.fontawesome.com/08626bfbba.js" crossorigin="anonymous"></script>
+     <!--Font Awesome script-->
+     <script src="https://kit.fontawesome.com/08626bfbba.js" crossorigin="anonymous"></script>
+    <!-- jquery link -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <title><?= $pageName; ?></title>
 </head>
 
