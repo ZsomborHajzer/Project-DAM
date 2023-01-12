e65295d9aad2aac54ed710ee207a59e02d8de6ce
unknown <user@DESKTOP-C4U7FPJ.corp.E3T.com>
2023-01-12 15:09:28 +0000

    fix: Connection to db

--- public/view/logInPage.php
+++ public/view/logInPage.php
@@ -81,6 +81,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             if ($userPw==$dbPassWd){
                 echo  '<script>alert("You are logged in")
                 </script>';
+
+                //refreshing
                 header("Refresh: 0");
                 
 
