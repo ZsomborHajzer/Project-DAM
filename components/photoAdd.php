2095ca18b2f03ee769eea9d3054a7af09dc9379e
Alves Bernardo <bernardo.correia.alves@student.nhlstenden.com>
2023-01-13 13:14:43 +0100

    Fix: path on upload files

--- components/photoAdd.php
+++ components/photoAdd.php
@@ -18,20 +18,20 @@ if (!is_dir($newPfrad)) {
 }
 
 
-if ($_FILES["uploadImg"]["error"] == 0) {
+if ($_FILES["profileFile"]["error"] == 0) {
 
-    if ($_FILES["uploadImg"]["size"] < $fileSize) {
+    if ($_FILES["profileFile"]["size"] < $fileSize) {
 
         $acceptedFileTypes = ["image/gif", "image/jpg", "image/jpeg", "image/png"];
 
         $fileinfo = finfo_open(FILEINFO_MIME_TYPE);
-        $uploadedFileType = finfo_file($fileinfo, $_FILES["uploadImg"]["tmp_name"]);
+        $uploadedFileType = finfo_file($fileinfo, $_FILES["profileFile"]["tmp_name"]);
 
         if (in_array($uploadedFileType, $acceptedFileTypes)) {
-            if (!file_exists($newPfrad . $_FILES["uploadImg"]["name"])) {
+            if (!file_exists($newPfrad . $_FILES["profileFile"]["name"])) {
 
                 //move_uploaded_file is a function that checks if the file was uploaded a secure way and if it was it will move it to the designated place. The first parameter checks if it was uploaded using a post mechanism, the second parameter transfers it to the designated file holder. If this function passes, it returns a true. if it does not it returns a false.
-                if (move_uploaded_file($_FILES["uploadImg"]["tmp_name"], $newPfrad . $_FILES["uploadImg"]["name"])) {
+                if (move_uploaded_file($_FILES["profileFile"]["tmp_name"], $newPfrad . $_FILES["profileFile"]["name"])) {
                     echo "ok";
                 } else {
                     echo "Something went wrong";
