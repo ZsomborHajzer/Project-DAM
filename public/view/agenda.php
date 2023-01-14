2ed8bdc1531884e72d243f655ff7a63d85f439d6
2560p <112290055+2560p@users.noreply.github.com>
2023-01-14 17:10:57 +0100

    Add: main calendar functionality

--- public/view/agenda.php
+++ public/view/agenda.php
@@ -1,13 +1,35 @@
 <?php
 
+// if we haven't got the id parameter
 if (empty($_GET["id"])) {
     header("Location: /");
+    die();
 }
+
+// if the id parameter is present, but it's not a number
+if (!is_numeric($_GET["id"])) {
+    header("Location: /");
+    die();
+}
+
+include "/var/www/E3T/components/dbConnect.php";
+
+// then we try to get the name. if no user found - redirect to the main page.
+$nameStmt = $dbHandler->prepare("SELECT dtFirstName, dtLastName FROM tblUser WHERE idUser = :id");
+$nameStmt->bindParam("id", $_GET["id"]);
+$nameStmt->execute();
+
+if ($nameStmt->rowCount() == 0) {
+    header("Location: /");
+    die();
+} else {
+    $talentName = $nameStmt[0] . " " . $nameStmt[1];
+}
+
 
 $pageName = "E3T-home";
 $cssFile = "calendarStyles.css";
 include "/var/www/E3T/components/header.php";
-// include "/var/www/E3T/components/dbConnect.php";
 
 ?>
 
@@ -15,9 +37,12 @@ include "/var/www/E3T/components/header.php";
 // $month = 12;
 // $year = 2022;
 
-$stamp = time();
-$start_day = date('N', $stamp);
-$end_day = date('t', $stamp);
+// $stamp = time();
+$start_day = date('N', strtotime("first day of this month"));
+$end_day = date('t');
+
+// echo date("Y-m-d", strtotime("first day of this month"));
+// echo date("Y-m-d", strtotime("last day of this month"));
 
 $weeks = array();
 $week = array();
@@ -55,8 +80,10 @@ for ($day = 1; $day <= $end_day; $day++) {
     }
 }
 
+
+
 ?>
-<h1 id="calendarTitle"><?php echo "Event calendar of "; ?></h1>
+<h1 id="calendarTitle"><?php echo "Event calendar of $talentName"; ?></h1>
 <div id="calenderMain">
     <table id="calenderTable">
         <thead>
@@ -75,10 +102,58 @@ for ($day = 1; $day <= $end_day; $day++) {
         </thead>
         <tbody>
             <?php
+            $query = $dbHandler->prepare("SELECT dtDate FROM tblBooking WHERE fiUser = :id AND dtDate > :start AND dtDate < :end");
+            $query->bindParam("id", $_GET["id"]);
+            $query->bindParam("start", strtotime("first day of this month"));
+            $query->bindParam("end", strtotime("last day of this month"));
+
+            $query->execute();
+            $arrBooked = $query->fetchAll(PDO::FETCH_COLUMN, 0);
+            $booked = [];
+            foreach ($arrBooked as $day) {
+                $i = explode("-", $day);
+                array_push($booked, (int) end($i));
+            }
+
+
+            $query = $dbHandler->prepare("SELECT dtStartDate, dtEndDate FROM tblAvaible WHERE fiUser = :id AND dtStartDate > :start dtStartDate < :end");
+            $query->bindParam("id", $_GET["id"]);
+            $query->bindParam("start", strtotime("first day of this month"));
+            $query->bindParam("end", strtotime("last day of this month"));
+
+            $query->execute();
+            $arrUnavailable = $query->fetchAll();
+            $unavailable = [];
+            foreach ($arrUnavailable as $entry) {
+                $startDay = explode("-", $entry[0]);
+                $endDay = explode("-", $entry[1]);
+                $start = (int) end($startDay);
+                $end = (int) end($endDay);
+
+                echo $start . '<br />';
+                echo $end . '<br />';
+
+                if ($start < $end) {
+                    for ($i = $start; $i <= $end; $i++) {
+                        array_push($unavailable, $i);
+                    }
+                } else {
+                    for ($i = $start; $i <= date("t"); $i++) {
+                        array_push($unavailable, $i);
+                    }
+                }
+            }
+
             foreach ($weeks as $week) {
                 echo "<tr>" . PHP_EOL;
                 foreach ($week as $day) {
-                    echo "<td>$day</td>" . PHP_EOL;
+                    if (in_array($day, $booked)) {
+                        echo "<td class='booked'>$day</td>" . PHP_EOL;
+                    } else if (in_array($day, $unavailable)) {
+                        echo "<td class='unavailable'>$day</td>" . PHP_EOL;
+                    } else {
+                        echo "<td class='free'>$day</td>" . PHP_EOL;
+                    }
                 }
                 echo "</tr>" . PHP_EOL;
             }
